dad2be83a7a1eca7bb267f3d3ba6772224f323c9
DhruvAvadhanam <1042772@lwsd.org>
2025-05-08 09:02:40 -0700

    Create bullseye.js
    Initial JavaScript Code

---
+++ m05/bullseye.js
@@ -0,0 +1,19 @@
+var input = prompt("What value do you want the multiplication table for? (0-10)");
+
+if (isNaN(input) || isNull(input)) {
+  input=0;
+}
+
+var msg = "<h2>Multiplication Table" + "</h2>";
+var output = msg;
+
+function multiplication() {
+  var index = 1;
+  while (index <= 10) {
+    output += index + " x " + input + " = " + (index * input) + "<br>";
+    index++;
+  }
+}
+
+multiplication();
+document.getElementById("blackboard").innerHTML = output;
